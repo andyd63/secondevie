5143abb374281a47bd0dc4ab8ef4812b8f5411eb
andy douarre <andydouarre@gmail.com>
2020-11-25 01:32:40 +0100

    ajout icone

--- assets/inc/header.php
+++ assets/inc/header.php
@@ -57,7 +57,7 @@ $configSite = initConfigSite();
 <script src="./js/commun.js"></script>
 <script src="js/jquery-1.11.3.min.js"></script>
 <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/bs/dt-1.10.22/sp-1.2.0/datatables.min.css"/>
- 
+
 <script type="text/javascript" src="https://cdn.datatables.net/v/bs/dt-1.10.22/sp-1.2.0/datatables.min.js"></script>
 <script type="text/javascript" src="https://cdn.datatables.net/buttons/1.6.4/js/dataTables.buttons.min.js"></script>
 <script type="text/javascript" src="https://cdn.datatables.net/buttons/1.6.4/js/buttons.flash.min.js"></script>
@@ -65,11 +65,8 @@ $configSite = initConfigSite();
 <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
 <script type="text/javascript" src="https://cdn.datatables.net/buttons/1.6.4/js/buttons.html5.min.js"></script>
 <script type="text/javascript" src="https://cdn.datatables.net/buttons/1.6.4/js/buttons.print.min.js"></script>
-
-
 <script src="https://cdn.tiny.cloud/1/bwkohbx6klhjryo3yzjrkhz2of52v8ir0chg0jyjj87guogd/tinymce/5/tinymce.min.js" referrerpolicy="origin"></script>
-<script src='https://cdn.tiny.cloud/1/bwkohbx6klhjryo3yzjrkhz2of52v8ir0chg0jyjj87guogd/tinymce/5/tinymce.min.js' referrerpolicy="origin">
-  </script>
+<link rel="icon" type="image/jpg" href="./assets/img/general/icon.png" />
   <script>
     tinymce.init({
       selector: '.mytextarea'

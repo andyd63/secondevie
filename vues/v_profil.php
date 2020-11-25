abc1a09218bb5d22bdb6f18fd99ab1a7d448c63d
andy douarre <andydouarre@gmail.com>
2020-11-25 13:29:46 +0100

    Ajout de la modification profil general

--- vues/v_profil.php
+++ vues/v_profil.php
@@ -27,46 +27,46 @@
               <div class="col-sm-8">
                 <?=genererError(22);?>
                 <p>
-                 <form action="index.php?c=inscription&action=valide" method="post">
+
                   <ul class="row">
                     
                     <!-- Name -->
                     <li class="col-md-6">
                       <label>Prénom
-                        <input type="text" name="prenom" value="<?=$cli->PRE_CLIENTS;?>"  disabled>
+                        <input type="text" name="prenom" class='not-allowed' value="<?=$cli->PRE_CLIENTS;?>"  disabled>
                       </label>
                     
                     </li>
                     <!-- LAST NAME -->
                     <li class="col-md-6">
                       <label>Nom
-                        <input type="text" name="nom" value="<?=$cli->NOM_CLIENTS;?>" disabled>
+                        <input type="text" name="nom" class='not-allowed' value="<?=$cli->NOM_CLIENTS;?>" disabled>
                       </label>
                     </li>
                     
                     <!-- EMAIL ADDRESS -->
                     <li class="col-md-6">
                       <label>Email
-                        <input type="email" name="email"  id="emailInscription"  value="<?=$cli->MAIL_CLIENTS;?>" disabled >
+                        <input type="email" name="email"  id="email" class='not-allowed'  value="<?=$cli->MAIL_CLIENTS;?>" disabled >
                       </label>
                     </li>
                     <!-- PHONE -->
                     <li class="col-md-6">
                       <label>Téléphone 
-                        <input type="tel" name="telephone" maxlength="10" value="<?=$cli->TEL_CLIENTS;?>" placeholder="" required >
+                        <input type="tel" name="telephone" id='tel' maxlength="10" value="<?=$cli->TEL_CLIENTS;?>" placeholder="" required >
                       </label>
                     </li>
                     <li class="col-md-6"> 
                       <!-- ADDRESS -->
                       <label>Adresse <span class="rouge"><span class="rouge">*</span></span>
-                        <input type="text" name="adresse" value="<?=$cli->ADRESSE;?>" placeholder="" required>
+                        <input type="text" name="adresse" id='adr' value="<?=$cli->ADRESSE;?>" placeholder="" required>
                       </label>
                     </li>      
                 
                     <!-- TOWN/CITY -->
                     <li class="col-md-6">
                       <label>Ville <span class="rouge"><span class="rouge">*</span></span>
-                        <input type="text" name="ville" value="<?=$cli->VILLE;?>" placeholder="" required>
+                        <input type="text" name="ville" id='ville' value="<?=$cli->VILLE;?>" placeholder="" required>
                       </label>
                     </li>
                     
@@ -74,17 +74,17 @@
                     <!-- TOWN/CITY -->
                     <li class="col-md-6">
                       <label>Code postal <span class="rouge"><span class="rouge">*</span></span>
-                        <input type="number" name="cp" value="<?=$cli->CODEPOSTAL;?>" placeholder="" required>
+                        <input type="number" name="cp"  id='cp' value="<?=$cli->CODEPOSTAL;?>" placeholder="" required>
                       </label>
                     </li>
                     
                     <!-- PHONE -->
                     <li class="col-md-12 textAlignCenter">
-                      <button id='btnInscription' type="submit" class="btn">Modifier</button>
+                      <button id='btnModifyGeneral' class="btn">Modifier</button>
                     </li>
                     
                   </ul>
-                </form>
+
               </div>
             </div>
           </div>
@@ -94,36 +94,19 @@
     
   <script>
     
-  $('#emailInscription').change(function(e){ 
-    Email = document.getElementById('emailInscription').value;
-    param = 'email='+Email
-    url= 'index.php?c=profil&action=verifMail&ajx=1&'+param;
+  $('#btnModifyGeneral').click(function(e){ 
+    Email = document.getElementById('email').value;
+    tel = document.getElementById('tel').value;
+    ville = document.getElementById('ville').value;
+    adr = document.getElementById('adr').value;
+    cp = document.getElementById('cp').value;
+    param = 'email='+Email+'&telephone='+tel+'&ville='+ville+'&adresse='+adr+'&cp='+cp;
+    url= 'index.php?c=profil&action=modif&ajx=1&'+param;
     messageRetour = '';
     reponse= postAjax(param,url,messageRetour);
     rep = reponse.responseText;
     rep = jQuery.parseJSON(rep);
-    if(rep.success){ // si le mail existe déjà
-        Swal.fire({
-                    icon: 'warning',
-                    title: 'Ce mail est déjà utilisé sur le site, veuillez prendre une autre adresse',
-      });
-      document.getElementById('btnInscription').disabled = true;
-    }else{
-      document.getElementById('btnInscription').disabled = false;
-    }
-  });
-  $('#mdpConfirm').change(function(e){ 
-    mdpConfirm = document.getElementById('mdpConfirm').value;
-    mdp = document.getElementById('mdpSimple').value;
-    if(mdp != mdpConfirm){ // si le mail existe déjà
-        Swal.fire({
-                    icon: 'warning',
-                    title: 'Les deux mots de passe ne sont pas identique!',
-      });
-      document.getElementById('btnInscription').disabled = true;
-    }else{
-      document.getElementById('btnInscription').disabled = false;
-    }
+    alertJs(rep.success,rep.msg);
   });
   
   </script>

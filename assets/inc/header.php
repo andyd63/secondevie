a81eb1c56eff0f5c3c3ad88fea0b8caf915ef889
andy douarre <andydouarre@gmail.com>
2020-11-27 23:55:42 +0100

    ajout https

--- assets/inc/header.php
+++ assets/inc/header.php
@@ -1,6 +1,10 @@
 <?php
-
-
+if (!(isset($_SERVER['HTTPS']) && ($_SERVER['HTTPS'] == 'on' || $_SERVER['HTTPS'] == 1) ||
+    isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] == 'https')) {
+    $redirect = 'https://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+   header('HTTP/1.1 301 Moved Permanently');
+   redirectUrl($redirect);
+   
 require_once('./modeles/m_bdd.php');
 require_once('./modeles/m_clients.php');
 require_once('./modeles/m_produit.php');

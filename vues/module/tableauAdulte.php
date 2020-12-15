18b7064f82791cffdd1d32adbcafe8eef399aee4
andy douarre <andydouarre@gmail.com>
2020-12-16 00:42:53 +0100

    Ajout texte qui présente les prix selon marque

--- vues/module/tableauAdulte.php
+++ vues/module/tableauAdulte.php
@@ -2,27 +2,31 @@
      <!-- Main Heading -->
         <div class="cart-ship-info margin-top-30">
           <h6 class="cursor shop-tittle margin-bottom-30" data-toggle="tooltip" data-placement="top" title="Voir le tableau"  onclick="changeVisibilite('tabAdulte','spanTabAdulte')"><span class="cursor" id="spanTabAdulte" ><i  class="fas fa-angle-up"></i></span> Tableau d'estimation prix d'achat adulte</h5>
-          <table id="tabAdulte" style='display:none'  class="table">
-                <thead>
-                  <tr>
-                    <th scope="col">Nom</th>
-                    <th scope="col">Prix normal</th>
-                    <th scope="col">Prix marque</th>
-                  </tr>
-                </thead>
-                <tbody>
-                <?php 
-                foreach($allProduitAdulte as $produitA){?>
-                  <tr>
-                    <th scope="row"><?= $produitA['nomSousCategorie'];?></th>
-                    <td><?= $produitA['prixAdulteLowCoast'];?>€</td>
-                    <td><?= $produitA['prixAdulteMarque'];?>€</td>
-                    <td></td>
-                  </tr>
-                  <?php }?>
+          <div id="tabAdulte" style='display:none' >
+            <p>Voici un exemple de marque pour le prix normal : Kiabi, celio, h&m ... </p>   
+            <p>Voici un exemple de marque pour le prix de marque : Devred, Adidas, Nike ... </p>
+              <table id="tabAdulte"  class="table">
+                    <thead>
+                      <tr>
+                        <th scope="col">Nom</th>
+                        <th scope="col">Prix normal</th>
+                        <th scope="col">Prix marque</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                    <?php 
+                    foreach($allProduitAdulte as $produitA){?>
+                      <tr>
+                        <th scope="row"><?= $produitA['nomSousCategorie'];?></th>
+                        <td><?= $produitA['prixAdulteLowCoast'];?>€</td>
+                        <td><?= $produitA['prixAdulteMarque'];?>€</td>
+                        <td></td>
+                      </tr>
+                      <?php }?>
+                      
                   
-              
-                </tbody>
+                    </tbody>
               </table>
+            </div>
           </div>
         
--- vues/module/tableauEnfant.php
+++ vues/module/tableauEnfant.php
@@ -1,8 +1,13 @@
 <hr>
      <!-- Main Heading -->
         <div class="cart-ship-info margin-top-30">
+         
+        
           <h6 class="cursor shop-tittle margin-bottom-30" data-toggle="tooltip" data-placement="top" title="Voir le tableau" onclick="changeVisibilite('tabEnfant','spanTabEnfant')"><span class="cursor" id="spanTabEnfant" ><i  class="fas fa-angle-down"></i></span> Tableau d'estimation prix d'achat enfant</h5>
-          <table id="tabEnfant" style='display:none' class="table">
+          <div id="tabEnfant" style='display:none' >
+          <p>Voici un exemple de marque pour le prix normal : Kiabi, celio, h&m ... </p>   
+          <p>Voici un exemple de marque pour le prix de marque : Devred, Adidas, Nike ... </p>
+          <table  class="table">
                 <thead>
                   <tr>
                     <th scope="col">Nom</th>
@@ -24,5 +29,6 @@
               
                 </tbody>
               </table>
+          </div>
           </div>
         
